a8729876333ed57ac6b0aa41a77dbc46e123ca50
liyang <389688501@qq.com>
2023-06-11 22:00:33 +0800

    feat:服务更新

--- server.py
+++ server.py
@@ -1,55 +1,58 @@
-import socket
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-from pydantic import BaseModel
-from pycorrector import Corrector
+# -*- coding: utf-8 -*-
+"""
+@author:David Euler
+@description:
+python3 -m pip install flask
+"""
+import sys
+from flask import Flask, request
 from loguru import logger
+import sys
+import time
+import os
+sys.path.append("..")
+from pycorrector import Corrector
+from pycorrector.macbert.macbert_corrector import MacBertCorrector
+pwd_path = os.path.abspath(os.path.dirname(__file__))
+startTime = time.time()
+app = Flask(__name__)
+rule_model = Corrector()
+rule_model.check_corrector_initialized()
+# macbert_model = MacBertCorrector()
+macbert_model = MacBertCorrector(pwd_path+"pycorrector/datasets/shibing624/macbert4csc-base-chinese")
+help = """
+You can request the service by HTTP get: <br> 
+   http://0.0.0.0:5001/macbert_correct?text=我从北京南做高铁到南京南<br>
 
-app = FastAPI()
-model = Corrector()
+or HTTP post with json: <br>  
+   {"text":"xxxx"} <p>
+Post example: <br>
+  curl -H "Content-Type: application/json" -X POST -d '{"text":"我从北京南做高铁到南京南"}' http://0.0.0.0:5001/macbert_correct
+"""
 
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-class InputParam(BaseModel):
-    text: str = None
+@app.route("/", methods=['POST', 'GET'])
+def hello_world():
+    return help
 
-@app.post("/TextCorrection/text")
-def text_correction(feed: InputParam):
-    try:
-        assert type(feed.text) is str
-        assert len(feed.text) > 0
-    except Exception as e:
-        logger.info('Parameter error, {}', e)
-        return {"code": 400,
-                "message": "参数类型异常"}
-    try:
-        corrected_sent, detail = model.correct(feed.text)
-        logger.info('Call interface, input is {}', feed.text)
-        return {"code": 200,
-                "corrected_text": corrected_sent,
-                "detail": detail,
-                "message": "请求成功"}
-    except Exception as e:
-        logger.info('Call interface, input is {}, Error is {}', feed.text, e)
-        return {"code": 500, "message": "请求失败"}
+@app.route('/macbert_correct', methods=['POST', 'GET'])
+def correct_api():
+    if request.method == 'POST':
+        data = request.json
+        logger.info("Received data: {}".format(data))
+        text = data["text"]
+        results = macbert_model.macbert_correct(text)
+        # return results[0] + " " + str(results[1])
+        return results[0]
+    else:
+        if "text" in request.args:
+            text = request.args.get("text")
+            logger.info("Received data: {}".format(text))
+            results = macbert_model.macbert_correct(text)
+            # return results[0] + " " + str(results[1])
+            return results[0]
+    return help
+
 
 if __name__ == '__main__':
-    def get_host_ip():
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            s.connect(('8.8.8.8', 80))
-            ip_ = s.getsockname()[0]
-        finally:
-            s.close()
-        return ip_
-
-    ip = get_host_ip()
-    uvicorn.run("service:app", host=ip, port=8010, reload=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
